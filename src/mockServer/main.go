0cada0a7bae50a2c8a4d8ceee8ed94cf24f9f0bc
a233894432 <a233894432@gmail.com>
2017-07-24 18:11:05 +0800

    增加 bolt.

--- src/mockServer/main.go
+++ src/mockServer/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
- 
+	"log"
+	"time"
+
+	"github.com/asdine/storm"
+	"github.com/boltdb/bolt"
+
 	"net/http"
- 
- 
+
 	"goji.io"
 	"goji.io/pat"
 )
-
- 
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	name := pat.Param(r, "name")
@@ -18,7 +20,13 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
- 	mux := goji.NewMux()
+	mux := goji.NewMux()
 	mux.HandleFunc(pat.Get("/hello/:name"), hello)
-	http.ListenAndServe("localhost:8000", mux)
+	db, err := storm.Open("db/my.db", storm.BoltOptions(0600, &bolt.Options{Timeout: 1 * time.Second}))
+	defer db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.ListenAndServe(":8000", mux)
 }
