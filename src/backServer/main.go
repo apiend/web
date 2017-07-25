f96a7c53c8d34117beaf16de8426665756a61621
a233894432 <a233894432@gmail.com>
2017-07-25 19:17:04 +0800

    jas server服务

--- src/backServer/main.go
+++ src/backServer/main.go
@@ -12,13 +12,22 @@ type Hello struct{}
 func (*Hello) Get(ctx *jas.Context) {
 	// `GET /v1/hello`
 	ctx.Data = "hello world"
-	
 	//response: `{"data":"hello world","error":null}`
 }
+
+type UsersId struct{}
+
+func (*UsersId) Photo(ctx *jas.Context) {
+	// `GET /users/:id/photo`
+	id := ctx.Id
+	ctx.Data = id
+}
+
 func main() {
-	router := jas.NewRouter(new(Hello))
+	router := jas.NewRouter(new(Hello), new(UsersId))
 	router.BasePath = "/v1/"
 	fmt.Println(router.HandledPaths(true))
+
 	//output: `GET /v1/hello`
 	http.Handle(router.BasePath, router)
 	http.ListenAndServe(":8080", nil)
