7daf1162c71a5da8b54acf8a1004f418552ca645
Diogoxiang <a233894432@gmail.com>
2017-08-04 00:33:29 +0800

    增加pongo2 的模板引擎

--- src/apiServer/main.go
+++ src/apiServer/main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"io"
 	// "log"
 	"net/http"
 
 	. "apiServer/conf"
 	"apiServer/module/log"
 
-	c "github.com/dchest/captcha"
-
+	"github.com/echo-contrib/pongor"
 	"github.com/hb-go/echo-mw/captcha"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 func main() {
 	confFilePath := "" // 默认conf/conf.toml
 
@@ -37,14 +44,32 @@ func main() {
 		SkipLogging: false,
 	}))
 
-	fmt.Println(c.NewLen(6))
+	// t := &Template{
+	// 	templates: template.Must(template.ParseGlob("public/views/*.html")),
+	// }
 
+	r := pongor.GetRenderer(pongor.PongorOption{
+		// 默认模版目录
+		Directory: "templates",
+		// if you want to reload template every request, set Reload to true.
+		Reload: true,
+	})
+	// fmt.Println(c.NewLen(6))
+	e.Renderer = r
 	// Route => handler
 	e.GET("/", func(c echo.Context) error {
-
 		return c.String(http.StatusOK, Conf.App.Name)
 	})
+
+	e.GET("/hello", Hello)
 	// log.Global.Printf("ddd")
+
 	// Start server
 	e.Logger.Fatal(e.Start(Conf.Server.Addr))
+}
+
+func Hello(c echo.Context) error {
+	return c.Render(200, "index.html", map[string]interface{}{
+		"title": "你好，世界",
+	})
 }
