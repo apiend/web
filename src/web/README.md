ef7ee70c02b9a1f094a310562f5581614e38d4a2
a233894432 <a233894432@gmail.com>
2017-10-30 15:18:16 +0800

    ...updata

---
+++ src/web/README.md
@@ -0,0 +1,5 @@
+## install
+glide get  github.com/elgs/jsonql
+glide get  github.com/tidwall/gjson
+ 
+glide get github.com/kataras/iris
---
+++ src/web/example/getJson.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/tidwall/gjson"
+
+const json = `{"name":{"first":"Janet","last":"Prichard"},"age":47}`
+
+func main() {
+	value := gjson.Get(json, "age")
+	println(value.String())
+}
